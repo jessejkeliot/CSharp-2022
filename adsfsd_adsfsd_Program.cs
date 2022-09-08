7e5c09614e832c48345b82e60cd113f9e8537487
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-08 20:35:54 +0100

    Add adsfsd_adsfsd_Program.cs

---
+++ adsfsd_adsfsd_Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace adsfsd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fart = "fart in my mouth";
+            Console.WriteLine(fart);
+            string response = Console.ReadLine();
+        }
+    }
+}
+
