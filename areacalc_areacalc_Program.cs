68627e7107e16481930feaf073ad1faf38cd1dab
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-14 15:37:16 +0100

    Add areacalc_areacalc_Program.cs

---
+++ areacalc_areacalc_Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace areacalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 32");
+            string userinp = Console.ReadLine();
+            int userinpint = Convert.ToInt32(userinp);
+            int counter = 0;
+            int current = 0;
+            int[] array1 = new int[32];
+            string currentstr = "";
+            int stringcount = 0;
+            double strnew = 0;
+            int length = 0;
+            while (counter <32)
+            {
+                //Console.WriteLine(current);
+                stringcount = 1;
+                strnew = 1;
+                current += 1;
+                currentstr = Convert.ToString(currentstr);
+                for (int i = 0; i < currentstr.Length; i++)
+                {
+                    char currentchar = currentstr[i];
+                    int now = currentchar - '0';
+                    length = currentstr.Length;
+                    Console.WriteLine(length);
+                    strnew = strnew + Math.Pow(now, length);
+                    //Console.WriteLine(strnew);
+                }
+
+                if (strnew == current)
+                {
+                    Console.WriteLine(current);
+                    array1[counter] = current;
+                    counter += 1;
+                }
+            }
+            Console.WriteLine("done");
+            System.Console.WriteLine(array1[userinpint]);
+        }
+    }
+}
+
