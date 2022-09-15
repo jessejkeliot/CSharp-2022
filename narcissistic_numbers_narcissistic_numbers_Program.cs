e165cbb0cbf268836f4f691c15ce0f15c933755e
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-15 10:47:44 +0100

    Add narcissistic_numbers_narcissistic_numbers_Program.cs

---
+++ narcissistic_numbers_narcissistic_numbers_Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace narcissistic_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int current = 1;
+            int total = 0;
+            int[] listofnumbs = new int[32];
+            while (counter<32)
+            {
+                total = 0;
+                foreach (char i in Convert.ToString(current))
+                {
+                    string digit = Convert.ToString(i);
+                    int newi = Convert.ToInt32(digit);
+                    string currentstr = Convert.ToString(current);
+                    int length = currentstr.Length;
+                    total += Convert.ToInt32(Math.Pow(Convert.ToDouble(newi), Convert.ToDouble(length)));
+                }
+                if (current == total)
+                {
+                    listofnumbs[counter] = total;
+                    Console.WriteLine(total);
+                    counter++;
+                }
+                current++;
+            }
+        }
+    }
+}
+
