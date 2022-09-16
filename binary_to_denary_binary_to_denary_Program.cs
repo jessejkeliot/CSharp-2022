3a8f6b846a4be3c0a9b4ccef3fb0141d0f4bc3e0
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-16 19:42:00 +0100

    Add binary_to_denary_binary_to_denary_Program.cs

---
+++ binary_to_denary_binary_to_denary_Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace binary_to_denary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                int answer = 0;
+                int column = 8;
+                do
+                {
+                    Console.WriteLine("Enter Bit Value");
+                    int bit = Convert.ToInt32(Console.ReadLine());
+                    answer += column * bit;
+                    column = column / 2;
+                }
+                while (column >= 1);
+                Console.WriteLine("Decimal value is: {0}", answer);
+            }
+        }
+    }
+}
+
