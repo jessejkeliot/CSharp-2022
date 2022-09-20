5ee2027c91eb662694b1b3aad2354473a3ede398
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-20 11:41:45 +0100

    Add dtob_dtob_Program.cs

---
+++ dtob_dtob_Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dtob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in the decimal");
+            string decimalstr = Console.ReadLine();
+            int answer = 123;
+            string binstr = "";
+            int remainder = 0;
+            int decimalint = Convert.ToInt32(decimalstr);
+            answer = decimalint;
+            while (answer!= 0)
+            {
+                remainder = answer % 2;
+                answer = answer / 2;
+                binstr = Convert.ToString(remainder) + binstr;
+            }
+            Console.WriteLine(binstr);
+        }
+    }
+}
+
