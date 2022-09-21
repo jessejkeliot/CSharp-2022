8e73bb55c3f5002dfb3c877e88727ac9b034bf0c
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-21 15:19:55 +0100

    Add Auth_Auth_Program.cs

---
+++ Auth_Auth_Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Auth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userIDs = {"sally_A", "mbp", "Fred", "Emanuel"};
+            string[] passwords = { "roadKill@23", "@LoveH3@rt", "Peac3&War", "Easy@S123" };
+
+            while (true)
+            {
+                Console.WriteLine(userIDs.Length);
+                Console.WriteLine("Please enter your user ID:  ");
+                string testUserID = Console.ReadLine();
+                Console.WriteLine("Please enter your password:  ");
+                string testPassword = Console.ReadLine();
+                bool userIDCorrect = false;
+                bool passwordCorrect = false;
+
+                for (int i = 0; i < userIDs.Length; i++)
+                {
+                    if (testUserID == userIDs[i])
+                    {
+                        userIDCorrect = true;
+                        if (testPassword == passwords[i])
+                        {
+                            passwordCorrect = true;
+                        }
+                    }
+                }
+                if (userIDCorrect && passwordCorrect)
+                {
+                    Console.WriteLine($"Welcome {testUserID}. Access Granted.");
+                }
+                else
+                {
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("********* Entry Denied *********");
+                    Console.WriteLine("UserID / Password not recognised");
+                    Console.WriteLine("********************************");
+                }
+            }
+        }
+    }
+}
+
