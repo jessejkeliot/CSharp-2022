87fb90b5fd464705408a04e781601bec7b9c8872
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-09-22 10:29:24 +0100

    Add codewaraalhpabetvalue_codewaraalhpabetvalue_Program.cs

---
+++ codewaraalhpabetvalue_codewaraalhpabetvalue_Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace codewaraalhpabetvalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "abc";
+            string answer = "";
+            char currentnum = ' ';
+            int currentint = 0;
+            int integer = 0;
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                integer = (int)text[i] - 96;
+                answer += " " + Convert.ToString(integer);
+                Console.WriteLine(text[i]);
+                Console.WriteLine(integer);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
+
