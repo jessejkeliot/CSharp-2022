837ef3f541c5cf46185d86e17855c4f4e4f79e66
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2022-12-08 10:24:21 +0000

    Add 12_days_of_xmAS_12_days_of_xmAS_Program.cs

---
+++ 12_days_of_xmAS_12_days_of_xmAS_Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+    class Program{
+        static void Main(string[] args){
+            string[] gift_list = { "partridge in a pear tree", "turtle doves", "French hens", "calling birds", "gold rings", "geese a-laying", "swans a-swimming", "maids a-milking", "ladies dancing", "lords a-leaping", "pipers piping", "drummers drumming" };
+            string[] ordinals = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "ninth", "tenth", "eleventh", "twelfth" };
+            string[] numbers = { "a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
+        for (int count = 1; count < 13; count++){
+            Console.WriteLine($"\nOn the {ordinals[count - 1]} day of christmas my true love sent to me");
+            for (int i = count; i > 0; i--){
+                if (i == 1 && count > 1) { Console.Write("and "); }
+                Console.WriteLine($"{numbers[i - 1]} {gift_list[i - 1]}");}}}}
+    
+
